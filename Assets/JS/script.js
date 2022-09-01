292568a4fe383b119a3eba0a28498b7a51c81e46
Chris <kjman26@gmail.com>
2022-08-31 21:14:07 -0700

    added images, questions, and answers in arrays

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,9 +3,15 @@ var time = document.getElementById('time')
 //all functions need to be on click?????????
 
 // questions for quiz split by '?' character
-var message = "why did the chicken cross the road? where are you now? what is your favorite food?" 
+var questions = ["What global phenom K-pop group do these members belong to?", "What is the popular alcoholic drink that combines soju and beer?", "How many out of the 9 members of the k-pop girl-group TWICE are Korean?", "What is the most famous Korean vacation-island?", "What famous K-drama are these two (now married) actors from?"]
+// answer sets for questions
+var answerSet1 = ["BTS", "Big Bang", "Stray Kids", "Seventeen"]
+var answerSet2 = ["Mul", "Somaek", "Maekju", "A Good Time"]
+var answerSet3 = ["1" , "3", "4", "6"]
+var answerSet4 = ["Oedo", "Hongdo", "Ulleungdo", "Jeju"]
+var answerSet5 = ["Vagabond", "Crash Landing on You", "Attorney Woo", "Prison Playbook"]
 
-var words = message.split('?')
+// timer
 
 function countdown(){
     var timeLeft = 5;
