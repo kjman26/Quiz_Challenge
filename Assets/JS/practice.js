0b878294a09130af6897b88991e5f030402f2f6c
Chris <kjman26@gmail.com>
2022-09-07 21:10:18 -0700

    updates to images and some features

---
+++ Assets/JS/practice.js
@@ -0,0 +1,9 @@
+var answers = document.getElementById(".stuff")
+var question = document.getElementById("question")
+
+var questions=[];
+var question ="";
+
+var quiz = ["question", "answer", "answer 2"];
+
+question.textContent = quiz[0]
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,36 +1,48 @@
-var timer = document.getElementById('countdown')
-var time = document.getElementById('time')
+var timerEl = document.getElementById("#countdown")
+var timerEl = document.getElementById("time")
 //all functions need to be on click?????????
 
-// questions for quiz split by '?' character
-var questions = ["What global phenom K-pop group do these members belong to?", "What is the popular alcoholic drink that combines soju and beer?", "How many out of the 9 members of the k-pop girl-group TWICE are Korean?", "What is the most famous Korean vacation-island?", "What famous K-drama are these two (now married) actors from?"]
-// answer sets for questions
-var answerSet1 = ["BTS", "Big Bang", "Stray Kids", "Seventeen"]
-var answerSet2 = ["Mul", "Somaek", "Maekju", "A Good Time"]
-var answerSet3 = ["1" , "3", "4", "6"]
-var answerSet4 = ["Oedo", "Hongdo", "Ulleungdo", "Jeju"]
-var answerSet5 = ["Vagabond", "Crash Landing on You", "Attorney Woo", "Prison Playbook"]
+//questions and answers, will be in OL
+var questionSet = []
+
+var questions=[
+    ["What global phenom K-pop group do these members belong to?","BTS", "Big Bang", "Stray Kids", "Seventeen"], 
+    ["What is the popular alcoholic drink that combines soju and beer?","Mul", "Somaek", "Maekju", "A Good Time"], 
+    ["How many out of the 9 members of the k-pop girl-group TWICE are Korean?","1" , "3", "4", "6"], 
+    ["What is the most famous Korean vacation-island?","Oedo", "Hongdo", "Ulleungdo", "Jeju"], 
+    ["What famous K-drama are these two (now married) actors from?","Vagabond", "Crash Landing on You", "Attorney Woo", "Prison Playbook"]
+];
+
+function showQuestion (){
+    document.getElementById("#questions").textContent = questionSet;
+    JSON.stringify(Q1)
+}
+showQuestion();
 
 // timer
 
 function countdown(){
     var timeLeft = 5;
     var timeInterval = setInterval(function(){
-        if (timeLeft > 5){
-            timer.textContent = timeLeft + "seconds remaining"
+        if (timeLeft > 1){
+            time.textContent = timeLeft + "seconds remaining"
             timeLeft--;
         }
         else if (timeLeft === 1){
-            time.textContent = timeLeft + "second remaining"
+            time.textContent = timeLeft + "second remaining";
+            timeLeft--;
         }
         else {
             timer.textContent = "";
             clearInterval(timeInterval);
-
+            clearInterval(timeInterval)
             // need function here to happen after timer hits zero
         }
 
-    },5000)
+    },1000)
 };
 
 countdown();
+
+//update scoreboard
+// next page on timeout
