2400bb388d41f2573af6b022074bd166f3a1fa5c
Chris <kjman26@gmail.com>
2022-09-08 17:35:54 -0700

    updated 1st html, css

--- Assets/JS/practice.js
+++ Assets/JS/practice.js
@@ -1,9 +1,35 @@
-var answers = document.getElementById(".stuff")
-var question = document.getElementById("question")
-
+var answers = document.getElementById("#stuff")
+var question = document.getElementById("#question")
+var test = document.getElementById("#Test")
 var questions=[];
 var question ="";
+var timerEl = document.getElementById("#countdown")
+var timerEl = document.getElementById("time")
 
-var quiz = ["question", "answer", "answer 2"];
+question.textContent('hello')
+// var quiz = ["question", "answer", "answer 2"];
 
-question.textContent = quiz[0]
+
+//timer - change to 60seconds - score = timeLeft = X.textcontent or something?
+function countdown(){
+    var timeLeft = 5;
+    var timeInterval = setInterval(function(){
+        if (timeLeft > 1){
+            time.textContent = timeLeft + "seconds remaining"
+            timeLeft--;
+        }
+        else if (timeLeft === 1){
+            time.textContent = timeLeft + "second remaining";
+            timeLeft--;
+        }
+        else {
+            timer.textContent = "";
+            clearInterval(timeInterval);
+            clearInterval(timeInterval)
+            // need function here to happen after timer hits zero
+        }
+
+    },1000)
+    
+    countdown();
+};
